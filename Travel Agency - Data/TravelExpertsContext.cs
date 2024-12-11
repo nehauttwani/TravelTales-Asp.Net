f2e9423101d5c28dbce71162ae3710f543ae8a4b
taiwoadejoro <taiwoadejoro@gmail.com>
2024-12-11 11:02:55 -0700

    registered Db context

--- Travel Agency - Data/TravelExpertsContext.cs
+++ Travel Agency - Data/TravelExpertsContext.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Travel_Agency___Data.Models;
 
+
+
 namespace Travel_Agency___Data;
 
-public partial class TravelExpertsContext : DbContext
-{
-    public TravelExpertsContext()
+public partial class TravelExpertsContext : IdentityDbContext<User> { 
+    public TravelExpertsContext() :base()
     {
     }
 
